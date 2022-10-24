57e0af577b5404c14f6ba3642d7ae4238192036b
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-24 19:55:12 +0530

    fix:#2 event changed

--- 02NameConversion/nameConversion.js
+++ 02NameConversion/nameConversion.js
@@ -10,26 +10,43 @@ function caseChanger(){
     const scr_snake = document.querySelector("#screaming-snake-case");
     const kebab = document.querySelector("#kebab-case");
     const scr_kebab = document.querySelector("#screaming-kebab-case");
-    input_text.addEventListener("keydown", (e)=>{
+    input_text.addEventListener("keyup", (e)=>{
         let content = input_text.value;
         content = content.split(" ");
-        const content_camel = getCamel(content);
-        camel.textContent = content_camel.join("");
-        pascal.textContent = content.map((ele)=> ele[0].toUpperCase()+ele.substring(1)).join("");
-        snake.textContent = content.map((ele)=> ele[0].toLowerCase()+ele.substring(1)).join("_");
+        content = content.filter((item)=> item != "");
+        const convertedObj = getConverted(content)
+        camel.textContent = convertedObj.camel.join("");
+        pascal.textContent = convertedObj.pascal.join("");
+        snake.textContent = convertedObj.snake.join("_");
+        scr_snake.textContent = convertedObj.sre_snake.join("_");
+        kebab.textContent = convertedObj.snake.join("-");
+        scr_kebab.textContent = convertedObj.sre_snake.join("-");
     });
 
 }
 
 caseChanger();
 
-function getCamel(content){
-    content.map((ele, index_)=>{
-        if(index_ !==0){
+function getConverted(content){
+    const conveted = {};
+    const camel = content.map((ele, index_)=>{
+        if(index_ !== 0){
             return ele[0].toUpperCase()+ele.substring(1);
         }
-        else {
+        else{
             return ele[0].toLowerCase()+ele.substring(1);
-        };
+        }
     });
+    const pascal = content.map((ele_)=> ele_[0].toUpperCase()+ ele_.substring(1));
+    const snake = content.map((ele_)=> ele_.toLowerCase());
+    const sre_snake = content.map((ele_)=> ele_.toUpperCase());
+
+
+    return {
+        camel,
+        pascal,
+        snake,
+        sre_snake
+    }
+    
 }
