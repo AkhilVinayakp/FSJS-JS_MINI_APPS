14e1930415751bb979ef838cbde928164927ec08
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-25 10:55:26 +0530

    feat: #5 ok fix: time delay update

--- 05RockPaperScissor/js/main.js
+++ 05RockPaperScissor/js/main.js
@@ -13,12 +13,16 @@ const currUserOption = document.getElementById("result-user-stat");
 const currCompOption = document.getElementById("result-comp-stat");
 const winnerOption = document.getElementById("result-final-stat");
 
+let user = 0;
+let computer = 0;
 const choices = document.querySelectorAll(".choice");
+const choice_container = document.querySelector(".choices");
 choices.forEach(element => {
     element.addEventListener("click", ()=>{
+        choice_container.classList.add('hidden');
         let options = ['r', 'p', 's'];
         const got_id = element.getAttribute("id");
-        alert(`id clicked ${got_id}`);
+        // alert(`id clicked ${got_id}`);
         // random picking the computer option.
         const comOption = options[Math.floor(Math.random()*options.length)];
         //updating the ui with the given options
@@ -31,10 +35,29 @@ choices.forEach(element => {
         compChoosenElemet.setAttribute("style", "margin-left: 24px");
         currCompOption.append(compChoosenElemet);
         // choosing the winner
-        const winner = chooseWinner(got_id, comOption);
-
-
-
+        if(got_id === comOption){
+            optionCopy.classList.add("choice-err");
+            compChoosenElemet.classList.add("choice-err");
+        }
+        else {
+            const isUserWinner = chooseWinner(got_id, comOption);
+            if(isUserWinner){
+                user += 1;
+                userScore.textContent = user;
+                winnerOption.textContent = "Winner: USER";
+            }
+            else{
+                computer += 1;
+                compScore.textContent = computer;
+                winnerOption.textContent = "Winner: COMP";
+            }
+        }
+        setTimeout(()=>{
+            currCompOption.removeChild(compChoosenElemet);
+            currUserOption.removeChild(optionCopy);
+            winnerOption.textContent = "";
+            choice_container.classList.remove("hidden");
+        },800);
     })
 });
 
@@ -43,7 +66,11 @@ choices.forEach(element => {
 
 
 function chooseWinner(uChoose, cChoose){
-    const r_wins = ['p'];
-    const p_wins = ['']
-
+    const r_wins = 's';
+    const p_wins = 'r';
+    const s_wins = 'p';
+    const result = (uChoose == 'r' && cChoose == r_wins) ? true :
+                 (uChoose == 'p' && cChoose == p_wins) ? true :
+                 (uChoose == 's' && cChoose == s_wins) ? true : false;
+    return result;
 }
